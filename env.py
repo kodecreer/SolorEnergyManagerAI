a5bbf8743ebab245d0faaa9be19d9099a2a22be2
Kode Creer <kode.creer@gmail.com>
2023-07-29 13:08:22 -0600

    made graph more efficient and created dummy model. Need addtional clarification on the buy sell hold with the data in order to continute

--- env.py
+++ env.py
@@ -4,17 +4,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import random
+import torch
+import torch.nn as nn
+
 #TASK List Sprint 1 ##########
 #TODO Create a dummy model   #
 #TODO Make sure able to train#
 #TODO Fix graph              #
 ##############################
+#TASK List Sprint 2 ##########
+#TODO Incorporate into TorchRL for parrallel envs
+#TODO Make graphing more efficient. Slows down for some reason after a while for no reason
+##############################
 
+class SolarAI(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(SolarAI, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.activation = nn.ReLU()  # You can choose any activation function you prefer
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        # Define the forward pass of your model
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
+        return x
 class SolarEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, data, initial_balance=10000):
         super(SolarEnv, self).__init__()
+
+        self.fig, self.ax = plt.subplots(2,2)
+        self.action_line,  = self.ax[0][0].plot([0], [0])
+        self.ax[0][0].set_xlabel('Timestep')
+        self.ax[0][0].set_ylabel('Balance')
+
+        self.vmp_line,  = self.ax[0][1].plot([0], [0])
+        self.ax[0][1].set_xlabel('Timestep')
+        self.ax[0][1].set_ylabel('VMP')
+
+        self.imp_line,  = self.ax[1][0].plot([0], [0])
+        self.ax[1][0].set_xlabel('Timestep')
+        self.ax[1][0].set_ylabel('IMP')
+
+        self.reward_line,  = self.ax[1][1].plot([0], [0])
+        self.ax[1][1].set_xlabel('Timestep')
+        self.ax[1][1].set_ylabel('Reward')
+        plt.ion()
+        plt.show(block=False)
 
         # Define your stock data, e.g., OHLC (Open, High, Low, Close) prices
         self.df = data
@@ -27,24 +66,50 @@ class SolarEnv(gym.Env):
 
         self.current_step = 0
         self.vimp = []
+        self.imp = []
         self.actions = []
+        self.rewards = []
 
     
-    def calc_reward(self):
+    def calc_reward(self, balance):
         #Price for rewards calcualtion
         #Constant rate = 1.0
-        #Convert VIMP into wattage
+       
+        return balance
+        
+    def get_price(self, vmp, imp, wattage_rate):
+         #Convert VIMP into wattage
         # Wattage * Constant = reward
-        pass
+        #Wattage (Pmax) = Vmp × Imp
+        #Implement logit for profit. This is good for now though
+        pmax = vmp * imp
+        reward = wattage_rate * pmax
+        return reward
     def step(self, action):
         # Implement the logic for one step in the environment
         # Perform the action (Buy, Sell, Hold), update balance, stock owned, etc.
         # Calculate reward and whether the episode is done
-
-        self.actions.append(random.randint(1, 22))
+        
         vimp = self.df['Vmp'][self.current_step]
+        imp = self.df['Imp'][self.current_step]
         self.vimp.append(vimp)
-        return None, 0, False, 0
+        self.imp.append(imp)
+        WATTAGE_RATE = 0.5 #Random number for 50 cents per wattage hour. EXPENSIVE!!!!
+
+        reward = self.get_price(vimp, imp, WATTAGE_RATE )
+        self.rewards.append(reward)
+
+        if action == 1:
+            #Sell
+            new_balance = self.actions[-1] + reward
+            self.actions.append(new_balance)
+        else:
+            #hold
+            value = self.actions[-1] if len(self.actions) > 0 else 0
+            self.actions.append(value)
+
+        #next observation
+        return (), reward, self.current_step >= len(self.df)-1, 0
 
     def reset(self):
         # Reset the environment to the initial state and return the observation
@@ -61,29 +126,29 @@ class SolarEnv(gym.Env):
         return np.array(first_value)
 
     def render(self, mode='human'):
-        # Implement the visualization of the environment using matplotlib
-        # plt.figure(figsize=(10, 6))
-        plt.subplot(1,2, 1)
-        # Plot rewards data
-        x = np.arange(len(self.actions))[-100:]
-        
-        plt.plot(x, self.actions[-100:], color='b')
-        plt.xlabel('Time')
-        plt.ylabel('Reward')
-        plt.title(f'Solar Management Environment (Step: {self.current_step})')
-        
-        plt.pause(0.000001)
-        #Show energy production
-        plt.subplot(1,2,2)
-        plt.plot(x, self.vimp[-100:], color='g')
-        plt.xlabel('Time')
-        plt.ylabel('Vimp Production')
-        plt.draw()
-        #Add IMP production
-        #Every possible graph relating to the rewards
+        log_val = 100
+        x_data = range(0, min(len(self.actions), log_val))
+        self.action_line.set_data(x_data, self.actions[-log_val:])
+        self.vmp_line.set_data(x_data, self.vimp[-log_val:])
+        self.imp_line.set_data(x_data, self.imp[-log_val:])
+        self.reward_line.set_data(x_data, self.rewards[-log_val:])
 
-        plt.pause(0.000001)
+        # Manually set the x-axis and y-axis limits
+        self.ax[0][0].set_xlim(min(x_data), max(x_data))
+        self.ax[0][0].set_ylim(min(self.actions[-log_val:]), max(self.actions[-log_val:]))
 
+        self.ax[0][1].set_xlim(min(x_data), max(x_data))
+        self.ax[0][1].set_ylim(min(self.vimp[-log_val:]), max(self.vimp[-log_val:]))
+
+        self.ax[1][0].set_xlim(min(x_data), max(x_data))
+        self.ax[1][0].set_ylim(min(self.imp[-log_val:]), max(self.imp[-log_val:]))
+
+        self.ax[1][1].set_xlim(min(x_data), max(x_data))
+        self.ax[1][1].set_ylim(min(self.rewards[-log_val:]), max(self.rewards[-log_val:]))
+
+        self.fig.canvas.draw()
+        plt.pause(1e-40)
+        
 
 
 if __name__ == '__main__':
@@ -95,6 +160,8 @@ if __name__ == '__main__':
 
     # Define the number of episodes (or time steps) you want to run the environment
     num_episodes = 1
+    log_interval = 90
+    interval = 0
 
     for episode in range(num_episodes):
         observation = env.reset()
@@ -105,9 +172,11 @@ if __name__ == '__main__':
 
             # Perform the chosen action in the environment
             next_observation, reward, done, _ = env.step(action)
-
-            # You can render the environment at each step if you want to visualize the progress
-            env.render()
+            interval += 1
+            if interval >= log_interval:
+                # You can render the environment at each step if you want to visualize the progress
+                env.render()
+                interval = 0
 
             # Update the current observation with the next observation
             observation = next_observation
